dbdd573127322fbdb0fb6b42d13d3c6bc286dcbc
kaushiknatua12345 <kaushiknatua12345@gmail.com>
2026-01-28 16:30:02 +0530

    Password Validation Push For Rebase

--- login.ts
+++ login.ts
@@ -1,6 +1,8 @@
 export function login(username: string, password: string) {
   if (!username) {
+  if (password.length < 8) {
     return false;
   }
-  return true;
+  return username && password;
+}
 }
