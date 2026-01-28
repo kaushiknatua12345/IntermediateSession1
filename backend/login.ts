7f7093ca224a2988a29c55da604cfc5d9d891e20
kaushiknatua12345 <kaushiknatua12345@gmail.com>
2026-01-28 18:41:43 +0530

    Code Change

--- backend/login.ts
+++ backend/login.ts
@@ -1,6 +1,6 @@
 export function login(username: string, password: string) {
-  if (password.length<11) {
-    return false;
+  if (!username || password.length>8) {
+    return true;
   }
-  return true;
+  return false;
 }
