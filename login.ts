d4610ebc32a7c565bcee448cbf80ecb223c4fbea
kaushiknatua <75721842+kaushiknatua12345@users.noreply.github.com>
2026-01-28 17:21:44 +0530

    Fix password length check in login function

--- login.ts
+++ login.ts
@@ -1,5 +1,5 @@
 export function login(username: string, password: string) {
-  if (!username && password.length<=9) {
+  if (!username && password.length<=7) {
     return false;
   }
   return true;
